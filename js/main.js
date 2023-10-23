a1f8a183db16a92c5bdefa6c2105ac94f5a90d20
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-10-23 13:56:25 -0400

    commit mostrar notícias

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ if ("serviceWorker" in navigator) {
     });
   }
 
-  const apiKey = '';
-  let url = ``;
+  const apiKey = '3d913c703e724b76b4a514f15b41402f';
+  let url = `https://newsapi.org/v2/everything?q=Apple&apiKey=${apiKey}`;
   const main = document.querySelector('main');
+
+  async function postNews() { 
+    const res = await fetch(url);
+    const data = await res.json();
+    main.innerHTML = data.articles.map(createArticle).join('\n');
+  }
+    
+    function createArticle (article) { 
+      console.log(article); 
+      return`
+              <div class="article">
+              <a href="${article.url})" target="_blank"> 
+              <img src="${article.urlToImage}" class="image" alt="${article.content}"/>
+              <h2>${article.title}</h2>
+              <p>${article.description}</p>
+              </a>
+              </div> 
+            `
+    }
